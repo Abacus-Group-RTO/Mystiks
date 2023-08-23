9c6df73c4c4195e913b71b8c8066a6993e5e2c33
Dennis Carlson <dcarlson@govanguard.com>
2023-08-23 09:47:55 -0500

    Prepare for distribution
    - Update version number to the planned release
    - Move CLI functionality out of __main__
    - Add build prerequisites

---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mystik',
+    version='1.0.0',
+    packages=find_packages(),
+    entry_points={
+        'console_scripts': [
+            'mystik = mystik:main'
+        ]
+    },
+    include_package_data=True,
+    author='Dennis Carlson',
+    author_email='dcarlson@gotham-security.com',
+    description='A Python-based, Rust-core secret searching tool ',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/GoVanguard/Mystik',
+    license='MIT',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3'
+    ],
+)
